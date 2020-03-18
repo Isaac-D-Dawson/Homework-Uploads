42004c67c5ab31c375aa251d747397fb7ba78b47
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-03-18 15:43:19 +0000

    created DivisibleBy3.py
    Contains a function and some test calls for that function. the function takes in a single input and returns True if that int is divisible wholly by 3. Otherwise, it returns false.

---
+++ Co-19-HW/DivisibleBy3.py
@@ -0,0 +1,15 @@
+#DivisibleBy3.py        18.03.2020
+
+def div_3(int):
+    if int % 3 == 0:
+        return(True)
+    else:
+        return(False)
+
+print(div_3(1))
+print(div_3(2))
+print(div_3(3))
+print(div_3(4))
+print(div_3(5))
+print(div_3(6))
+print(div_3(7))
