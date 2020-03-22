37a3e33f0c6a9543ba0e89845f36696c43feb6d4
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-03-22 21:06:21 +0000

    Created ShortCode.py
    Contaisna  single function and four test calls. The function takes in a list and returns the same list, with each entry in string format.

---
+++ Co-19-HW/ShortCode.py
@@ -0,0 +1,9 @@
+#ShortCode.py       22.03.2020
+
+def convert(list):
+        return([str(i) for i in list])
+
+print(convert([1,2,3,4]))
+print(convert(["a", "b", "c" "d"]))
+print(convert([1,6,4,3,]))
+print(convert([1]))
