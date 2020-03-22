d9ee65a08a65b197519167caa911b86e87a1d41b
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-03-22 19:45:05 +0000

    created UpAndDown.py
    Contains a single function and six test calls. The function takes in a single number, int or float, and returns a tuple containing one less than, and one more than the input number.

---
+++ Co-19-HW/UpAndDown.py
@@ -0,0 +1,11 @@
+#UpAndDown.py       22.03.2020
+
+def up_down(num):
+    return(tuple([num-1, num+1]))
+
+print(up_down(3))
+print(up_down(5))
+print(up_down(7))
+print(up_down(3.13159))
+print(up_down(0))
+print(up_down(-5))
