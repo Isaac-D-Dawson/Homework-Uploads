82b3488a57b83b8145f35ec3b59c0f49560366e7
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-03-29 16:41:51 +0100

    Updated CapitalIndexes.py
    Updated to include comments that explain the code and my thought process while writing it, along with tweaks to the test calls.

--- Co-19-HW/CapitalIndexes.py
+++ Co-19-HW/CapitalIndexes.py
@@ -1,13 +1,21 @@
 #CapitalIndexes/py      18.03.2020
 
+#Define our function and make it take a string as input.
 def capital_indexes(string):
+    #make sure we have a lowercase copy of the string for checking.
     lowerString = string.lower()
+    #Allocate our output list for later.
     output = []
+    #Create a loop and cuc;e through every letter in the string.
     for i in range(len(string)):
+        #Here we're comparing the uppercase letters and the lowercase letters.
         if string[i] != lowerString[i]:
+            #if they're not equal, we save their index to the output, as they must be capital letters.
             output.append(i)
+    #Then we return the output list.
     return(output)
 
-print(capital_indexes("HeLlO"))
-print(capital_indexes("HELLo"))
-print(capital_indexes("Hi World!"))
+#Test calls.
+print(capital_indexes("HeLlO")) #Returns "0, 2, 4", as those chars are capitallised.
+print(capital_indexes("HELLo")) #Returns "0, 1, 2, 3", as those chars are capitalised,
+print(capital_indexes("Hi World!")) #Returns "0, 3", as those chars are capitalised.
