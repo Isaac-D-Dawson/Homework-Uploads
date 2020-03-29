fadb8964c6d985cacff90ac69d128166bd9aa4bf
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-03-29 15:16:13 +0100

    Updated Acronym.py
    This update adds a large number of comments to the code, to carify to other readers what each section is doing and what my thought process was. I also reformatted the test calls to include comments with the outputs.

--- Co-19-HW/Acronym.py
+++ Co-19-HW/Acronym.py
@@ -1,15 +1,26 @@
 #Acronym.py     23.03.2020
 
+#Define function "Acronym" which takes in a single string.
 def acronym(string):
+    #Define a variable to help check if a letter is the first in a word
     first_tag = False
+    #Save the first letter of the string, capitalised.
     output = string[0].upper()
+    #For every letter in the string except the first one...
     for i in range(1, len(string)):
+        #if a letter in the string is a space...
         if string[i] == " ":
+            #...Then the next one is the first in a word...
             first_tag = True
+            #....and if it's the first letter...
         elif first_tag == True:
+            #...Save it, capitalised, to our output.
             output = output + string[i].upper()
+            #And reset to make sure we don't keep identifying.
             first_tag = False
+        #And once we've done that for all the letters in the string, return it.
     return(output)
 
-print(acronym("This is a long string"))
-print(acronym("I hope it's not an issue"))
+#Test calls:
+print(acronym("This is a long string")) #TIALS
+print(acronym("I hope it's not an issue")) #iHINAS
