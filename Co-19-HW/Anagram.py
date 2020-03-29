9f4ef2d78a9e73663e683a81ec56cb92f7f1f3f9
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-03-29 16:27:11 +0100

    Updated Anagram.py
    Updated to include comments that explain the code and my thought process while writing it, along with tweaks to the test calls.

--- Co-19-HW/Anagram.py
+++ Co-19-HW/Anagram.py
@@ -1,12 +1,16 @@
 #Anagram.py     23.03.2020
 
+#Define our function, and make it take in two stirngs. (I chose ana and ng at random from a song).
 def is_anagram(ana, ng):
+    #compare sorted copies of the strings. if they're equal that means the strings contain the same letters.
     if sorted(ana) == sorted(ng):
+        #and if they contain the same letters in the same amounts, they're anagramable, so we return true.
         return(True)
     else:
+        #if they aren't equal they aren't anagrams, so we retun false.
         return(False)
 
-print(is_anagram("", ""))
-print(is_anagram("typhoon", "opython"))
-print(is_anagram("Typhoon", "Opython"))     #doesn't compensate for captialiseation
-print(is_anagram("Alice", "Bob"))
+print(is_anagram("", ""))                   #Check to see how it handles empty strings. Returns True
+print(is_anagram("typhoon", "opython"))     #Test to make sure the logic works. Returns True
+print(is_anagram("Typhoon", "Opython"))     #doesn't compensate for captialiseation. Returns False
+print(is_anagram("Alice", "Bob"))           #Two totally different strings to make sure it won't catch on a false positives.
