d9573247c883e06997348f6783e57311914b42b6
Isaac-D-Dawson <little_tornado@hotmail.co.uk>
2020-09-16 12:59:50 +0100

    Created PopularWords.py
    Contains a function that takes a string and a list of substrings. It retuns a disctionary containing the number of times each substring occured in the main string. Keep in mind it is currently limited to words, seperated by spaces, case insensitive.

--- PyCheckIO/PopularWords.py
+++ PyCheckIO/PopularWords.py
@@ -22,7 +22,7 @@ def popular_words(text: str, words: list) -> dict:
     inval = text[0]
     
     for i in text:
-        text[text.index(i)] = f"{text[text.index(i)]} "
+        text[text.index(i)] = f" {text[text.index(i)]} "
     
     for i in text[1:]:
         inval = f"{inval}{i}"
@@ -36,3 +36,27 @@ def popular_words(text: str, words: list) -> dict:
         output[i] = inval.count(i)
     
     return(output)
+
+
+# if __name__ == '__main__':
+#     print("Example:")
+#     print(popular_words('''
+# When I was One
+# I had just begun
+# When I was Two
+# I was nearly new
+# ''', ['i', 'was', 'three', 'near']))
+
+#     # These "asserts" are used for self-checking and not for an auto-testing
+#     assert popular_words('''
+# When I was One
+# I had just begun
+# When I was Two
+# I was nearly new
+# ''', ['i', 'was', 'three', 'near']) == {
+#         'i': 4,
+#         'was': 3,
+#         'three': 0,
+#         'near': 0
+#     }
+#     print("Coding complete? Click 'Check' to earn cool rewards!")
